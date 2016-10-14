c72ba92753d2fb4b3133058d6183fd59d5778e3c
JD Gonzalez <jdg99@live.com>
2016-10-14 16:45:02 +0000

    empty upload dir

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var multer  = require('multer');
+var rmdir = require('rmdir');
 var path = require('path');
+var fs = require('fs');
 var upload = multer({ dest: 'uploads/' });
 var app = express();
 
@@ -8,13 +10,15 @@ app.set('json spaces', 4);
 
 app.post('/upload',  upload.single('fileToUpload'), function(req, res, next) {
   if (req.file) {
-    console.dir(req.file);
     return res.json({"File Size" : req.file.size});
   }
   res.end('Missing file');
 });
 
 app.get('/*', function(req, res) {
+    rmdir(__dirname + '/uploads/', function () {
+      fs.mkdir(__dirname + '/uploads/');
+    });
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
